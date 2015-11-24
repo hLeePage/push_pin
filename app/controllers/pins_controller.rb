d82f41045425dc286c2618ad55b1ade15f16bb4d
hLeePage <hLeepage@yahoo.com>
2015-11-24 14:05:40 -0600

    Add Edit and Destroy to Pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
   def index
-    @pins = Pin.all.order(created_at: :DESC)
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def show
@@ -16,12 +16,27 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
     if @pin.save
-      redirect_to @pin, notice: "Successfuly created new pin!"
+      redirect_to @pin, notice: "Successfully created new pin!"
     else
       redirect_to 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was successfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end
 
   private
 
